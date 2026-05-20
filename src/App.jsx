50b729d9401e99d2059a70ea1b668a679a2bbd02
ozturkdll <miahertz0@gmail.com>
2026-05-20 04:25:10 +0300

    render server baglantisi eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://kanka-okey-server.onrender.com");
 
 function App() {
   const [name, setName] = useState("");
